8dee211af3e1048e0249bd33e5cc427a0556a933
Sergio Giraldo <Sergir10@gmail.com>
2016-05-03 19:00:08 -0500

    Cambio en el hola mundo para correr en una página web

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http" //package for http based web programs
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Inside handler")
+	fmt.Fprintf(w, "Hello world from my Go program!")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", handler)              // redirect all urls to the handler function
+	http.ListenAndServe("localhost:9999", nil) // listen for connections at port 9999 on the local machine
 }
