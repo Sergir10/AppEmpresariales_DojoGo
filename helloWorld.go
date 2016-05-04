c92425e76b3a1b29e834c30dead8b07448841473
Sergio Giraldo <Sergir10@gmail.com>
2016-05-03 19:34:33 -0500

    Tercer intento con go

--- helloWorld.go
+++ helloWorld.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http" //package for http based web programs
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Inside handler")
-	fmt.Fprintf(w, "Hello world from my Go program!")
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)              // redirect all urls to the handler function
-	http.ListenAndServe("localhost:9999", nil) // listen for connections at port 9999 on the local machine
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
